04b34079176bedcc04d68a7eeaadd486aa19d119
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-10-14 17:16:08 -0300

    feat: Add route to users controller

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from app.domains.enterprise.controller import enterprise_router
 from app.domains.ia_group.controller import ia_group_router
 from app.domains.agent.controller import agent_router
 from app.domains.tool.controller import tool_router
+from app.domains.user.controller import user_router
 from app.api.exception_handlers import register_exception_handlers
 
 from fastapi import FastAPI
@@ -12,8 +13,12 @@ app = FastAPI(
     version='0.1.0'
 )
 
+# --- HTTP Routes ---
 app.include_router(enterprise_router)
 app.include_router(ia_group_router)
 app.include_router(agent_router)
 app.include_router(tool_router)
+app.include_router(user_router)
+
+# --- Exception Handlers ---
 register_exception_handlers(app)
