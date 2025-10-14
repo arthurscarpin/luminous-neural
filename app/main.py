fdec0ea0da98760ab61f3b7dca8a32615aed86d4
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-10-14 16:13:24 -0300

    feat: Add the tool router to the main application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from app.domains.enterprise.controller import enterprise_router
 from app.domains.ia_group.controller import ia_group_router
 from app.domains.agent.controller import agent_router
+from app.domains.tool.controller import tool_router
 from app.api.exception_handlers import register_exception_handlers
 
 from fastapi import FastAPI
@@ -14,4 +15,5 @@ app = FastAPI(
 app.include_router(enterprise_router)
 app.include_router(ia_group_router)
 app.include_router(agent_router)
+app.include_router(tool_router)
 register_exception_handlers(app)
